dbafee0e7937776db29bb8760a43214921caceca
xxxx <xxxxxx@gmail.com>
2023-01-06 17:30:29 +0900

    first commit

--- django_prj/settings.py
+++ django_prj/settings.py
@@ -137,3 +137,21 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
