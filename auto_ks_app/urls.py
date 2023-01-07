532f5d7e4c6be751e159a6add4fc2d39718dbdd1
xxxx <xxxxxx@gmail.com>
2023-01-07 10:31:50 +0900

    first commit

--- auto_ks_app/urls.py
+++ auto_ks_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'auto_ks_app'
 urlpatterns = [
     path('', views.img_up, name='img_up'),
-    path('transform/', views.transform, name='transform'),
+    # path('transform/', views.transform, name='transform'),
 ]
